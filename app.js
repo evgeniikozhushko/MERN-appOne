fc7f6a0077e490a61aee761266de6065810b0c26
evgeniikozhushko <climbersrock.eugene@gmail.com>
2025-06-23 17:36:42 -0600

    Fixed User model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const app = express();
+const UserModel = require('./models/User');
 
 require("./passport-config")(passport);
 
@@ -40,59 +41,59 @@ async function readUser() {
 }
 
 // Update User function
-async function updateUser(age) {
-  try {
-    const result = await UserModel.updateOne(
-      { name: "User 1" },
-      { $unset: { age: "" } }
-    );
-    console.log("User update result", result);
-  } catch (error) {
-    console.error("Error updating user", error);
-  }
-}
+// async function updateUser(age) {
+//   try {
+//     const result = await UserModel.updateOne(
+//       { name: "User 1" },
+//       { $unset: { age: "" } }
+//     );
+//     console.log("User update result", result);
+//   } catch (error) {
+//     console.error("Error updating user", error);
+//   }
+// }
 
 // Delete User function
-async function deleteUser() {
-  try {
-    const result = await UserModel.deleteOne({ name: "User 3" });
-    console.log("Delete result", result);
-  } catch (error) {
-    console.error("Error deleting user", error);
-  }
-}
+// async function deleteUser() {
+//   try {
+//     const result = await UserModel.deleteOne({ name: "User 3" });
+//     console.log("Delete result", result);
+//   } catch (error) {
+//     console.error("Error deleting user", error);
+//   }
+// }
 
 // Chain sorting and limiting Users age
-async function sortAndLimitUser() {
-  try {
-    const result = await UserModel.find().sort({ age: -1 }).skip(2).limit(3);
-    console.log("Found 3 oldest users", result);
-  } catch (error) {
-    console.log("Error sorting by age", error);
-  }
-}
+// async function sortAndLimitUser() {
+//   try {
+//     const result = await UserModel.find().sort({ age: -1 }).skip(2).limit(3);
+//     console.log("Found 3 oldest users", result);
+//   } catch (error) {
+//     console.log("Error sorting by age", error);
+//   }
+// }
 
 // Aggregation Pipeline of Users age
-async function aggAndPipe() {
-  try {
-    const result = await UserModel.aggregate([
-      { $group: { _id: null, averageAge: { $avg: "$age" } } },
-    ]);
-    console.log("Aggregation result:", result);
-  } catch (error) {
-    console.log("Aggregation Pipeline error", error);
-  }
-}
+// async function aggAndPipe() {
+//   try {
+//     const result = await UserModel.aggregate([
+//       { $group: { _id: null, averageAge: { $avg: "$age" } } },
+//     ]);
+//     console.log("Aggregation result:", result);
+//   } catch (error) {
+//     console.log("Aggregation Pipeline error", error);
+//   }
+// }
 
 // Function Calls
 createUser("User 1", 21, "female");
 createUser("User 2", 45, "male");
 createUser("User 3", 14, "male");
 readUser();
-updateUser(35);
-deleteUser();
-sortAndLimitUser();
-aggAndPipe();
+// updateUser(35);
+// deleteUser();
+// sortAndLimitUser();
+// aggAndPipe();
 
 app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: false }));
--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 // 1.	Local Authentication
 const UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    name: String,
+    age: Number,
+    gender: String
 })
 
 module.exports = mongoose.model('User', UserSchema)
